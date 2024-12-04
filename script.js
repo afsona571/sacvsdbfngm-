c7427309db2b486034e899bad5e7afb3facb283b
afsona571 <afsonaa59@gmail.com>
2024-12-04 18:29:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//  for (let i = 0; i < 100; i++) {
+//      console.log({i});
+//  }
+ //for (let i = 0; i < 100; i++) {
+   // if (i % 2 == 0)
+   // console.log({i});}
+
+   let newar=[]
+  let array =["non", "banan", "moshina", 'aizia',"olma", "asal",]
+    for (let i = 0; i< array.length; i++){
+       
+      if(array[i].includes('n')){
+console.log(array[i]);
+
+        console.log("n sozi ishtorok etgan sozlar"+  newar);
+        
+      }else{
+       console.log("n sozi ishtorok etmagan sozlar"+  array[i])
+
+      }
+       
+    }
+
+
+
+
+
+
+  
